7beb897eb3f9069d1207ecb5b0392698d2ccc6ca
nifasabila <109188525+nifasabila@users.noreply.github.com>
2022-08-24 14:53:45 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,10 @@
+<?php
+//pembuatan fungsi
+$panjang =18;
+$lebar =10;
+$tinggi =12;
+funCtion VolumeBalok ($panjang, $lebar,$tinggi) {
+    return ($panjang*$lebar*$tinggi);
+}
+//menampilkan fungsi
+echo "Volume balok tersebut adalah :" .VolumeBalok($panjang, $lebar,$tinggi). "cm";
